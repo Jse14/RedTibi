3d3b981f706e732cbab1da7dc1d175f4a47f2072
Kitrut <mmh30@alu.ua.es>
2019-05-13 18:01:01 +0200

    prueba mapa open layers

--- RedTibi/src/app/publicacion/publicacion.module.ts
+++ RedTibi/src/app/publicacion/publicacion.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { PublicacionPage } from './publicacion.page';
 
+
 const routes: Routes = [
   {
     path: '',
--- RedTibi/src/app/publicacion/publicacion.page.ts
+++ RedTibi/src/app/publicacion/publicacion.page.ts
@@ -2,13 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
 import { Publi } from '../publi';
 
+import OlMap from 'ol/Map';
+import OlXYZ from 'ol/source/XYZ';
+import OlTileLayer from 'ol/layer/Tile';
+import OlView from 'ol/View';
+import {fromLonLat} from 'ol/proj.js';
+import {defaults as defaultControls, FullScreen, OverviewMap} from 'ol/control.js';
+
 @Component({
   selector: 'app-publicacion',
   templateUrl: './publicacion.page.html',
   styleUrls: ['./publicacion.page.scss'],
 })
 export class PublicacionPage implements OnInit {
-
+  showMap=false;
+  map: OlMap;
+  source: OlXYZ = null;
+  layer: OlTileLayer;
+  view: OlView;
   titulo=null;
   p:Publi = new Publi();
   constructor(private navParams:NavParams,private modalController: ModalController) { }
@@ -18,10 +29,40 @@ export class PublicacionPage implements OnInit {
     this.p.subtitle = this.navParams.get('subtitulo');
     this.p.numLikes = this.navParams.get('numLikes');
     this.p.numComents = this.navParams.get('numComents');
-
   }
   closeModal(){
     this.modalController.dismiss();
+  }
+
+  initMap(){
+    if(this.source!=null){
+      return;
+    };
+    
+    this.source = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
+
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    this.view = new OlView({
+      center: fromLonLat([-0.5133,38.38504]),
+      zoom:16 ,
+      minZoom: 2,
+      maxZoom: 20
+    });
+
+    this.map = new OlMap({
+      layers: [this.layer],
+      controls: defaultControls().extend([
+        new FullScreen(),
+        new OverviewMap()
+      ]),
+      target: 'map',
+      view: this.view
+    });
   }
 
 }
