bab17eea3542f253604bad69869c83065039f845
Kitrut <mmh30@alu.ua.es>
2019-05-13 19:24:33 +0200

    Detalles de un evento

--- RedTibi/src/app/publicacion/publicacion.page.ts
+++ RedTibi/src/app/publicacion/publicacion.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
 import { Publi } from '../publi';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-publicacion',
@@ -8,7 +9,10 @@ import { Publi } from '../publi';
   styleUrls: ['./publicacion.page.scss'],
 })
 export class PublicacionPage implements OnInit {
-
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
   titulo=null;
   p:Publi = new Publi();
   constructor(private navParams:NavParams,private modalController: ModalController) { }
@@ -22,6 +26,22 @@ export class PublicacionPage implements OnInit {
   }
   closeModal(){
     this.modalController.dismiss();
+  }
+
+  verComentarios = true;
+  verAsistentes = false;
+  verCompartir = false;
+
+  segmentChanged(ev: any){
+    this.verComentarios=false;
+    this.verAsistentes=false;
+    this.verCompartir=false;
+    switch(ev.detail.value){
+      case "verComentarios": this.verComentarios=true;return;
+      case "verAsistentes": this.verAsistentes=true;return;
+      case "verCompartir": this.verCompartir=true;return;
+      default:return;
+    }
   }
 
 }
